b6bd1641f875e428c11c6f62e8c4eb2e249e5372
Daniel Kim <daniel.kim1116@gmail.com>
2016-01-14 18:45:26 -0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+    
