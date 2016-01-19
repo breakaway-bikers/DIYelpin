0a11740ae417b209176131e0e0c8db5e4c627260
Juan <jguardado85@gmail.com>
2016-01-18 19:01:38 -0800

    trying to get connected correctly

--- client/CreatePost/createPostController.js
+++ client/CreatePost/createPostController.js
@@ -4,41 +4,42 @@ angular.module('yelpin.createPost', [])
   //this should be set when user is signed in.
   $scope.username = '';
   $scope.discription = '';
+
   //not sure if this is necessary if we are storing info in the database
   $scope.comment = [];
-  $scope.txtcomment ='';
+  $scope.txtcomment = '';
 
-  $scope.postToPage = function($scope){
-    if($scope.txtcomment !=''){
+  $scope.postToPage = function($scope) {
+    if ($scope.txtcomment != '') {
       console.log($scope.txtcomment);
       $scope.comment.push($scope.txtcomment);
-      $scope.txtcomment = "";
+      $scope.txtcomment = '';
     }
+
     //this might need work. I'm attempting to send the information to the server using the factory function
     var data = { message: $scope.txtcomment };
-      appFactory.setPost().then(function(data) {
+    appFactory.setPost().then(function(data) {
     });
   };
 
   //using angular file uploader..research more
-  var uploader = new FileUploader({
-    filters: [{
-    name: 'yourName1',
-    // A user-defined filter
-      fn: function(item) {
-        return true;
-      }
-    }]
-  });
+  // var uploader = new FileUploader({
+  //   filters: [{
+  //   name: 'yourName1',
+  //   // A user-defined filter
+  //     fn: function(item) {
+  //       return true;
+  //     }
+  //   }]
+  // });
 
   //more angular file uploader
   //Another user-defined filter
-  uploader.filters.push({
-    name: 'yourName2',
-      fn: function(item) {
-        return true;
-      }
-    });
+  // uploader.filters.push({
+  //   name: 'yourName2',
+  //     fn: function(item) {
+  //       return true;
+  //     }
+  //   });
 
-    };
 }]);
