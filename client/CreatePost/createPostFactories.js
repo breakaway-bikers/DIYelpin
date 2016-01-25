105d0fab052cec8fd9133680cfb3a8e5da58a7cd
Juan <jguardado85@gmail.com>
2016-01-25 09:51:55 -0800

    passes user

--- client/CreatePost/createPostFactories.js
+++ client/CreatePost/createPostFactories.js
@@ -4,6 +4,10 @@ angular.module('yelpin.factory', [])
 
   var setPost = function(message) {
     //I've got to fix this pathway
+    return $http.post('/createPost', message).then(function(response) {
+      return response.data;
+      console.log(response.data);
+  });
 
   };
 
